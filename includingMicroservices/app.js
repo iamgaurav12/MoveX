b9773b424cbabb046d83e1a88088f9782f25a73c
iamgaurav12 <prakashgaurav189@gmail.com>
2025-02-20 02:19:56 +0530

    Learns how heavy traffic fails my system now I'm starting microservices

--- includingMicroservices/app.js
+++ includingMicroservices/app.js
@@ -1,12 +1,19 @@
-const express = require('express')
-
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
 
-app.get('/',(res,req)=>{
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  for (let i = 0; i < 10000000000; i++) {}
+  res.send("hello world");
+});
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000')
-})
+app.get("/stress-test", (req, res) => {
+  for (let i = 0; i < 10000000000; i++) {}
+  res.send("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ includingMicroservices/test/test.js
@@ -0,0 +1,24 @@
+const autocannon = require("autocannon");
+
+const urls = ["http://localhost:3000", "http://localhost:3000/stress-test"];
+const duration = 30; // in seconds
+
+urls.forEach((url) => {
+    const instance = autocannon(
+        {
+            url,
+            duration,
+        },
+        (err, results) => {
+            if (err) {
+                console.error("Error:", err);
+            } else {
+                console.log(`URL: ${url}`);
+                console.log("Number of Requests:", results.requests.total);
+                console.log("Duration (seconds):", results.duration);
+            }
+        }
+    );
+
+    autocannon.track(instance, { renderProgressBar: false, renderResultsTable: false });
+});
